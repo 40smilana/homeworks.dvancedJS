4b4386058dd56fb316a22437e7224b9bf15da2f6
40smilana <40smilana@gmail.com>
2025-01-30 15:26:14 +0300

    Создала модуль comments_loader.js, в нем реализую сообщения о загрузке старых комментариев и добавление новых

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { renderFunction } from './modules/сomment_rendering_function.js'
 import { likeButtonFunction } from './modules/listener_likeButton.js'
 import { fetchComments } from './modules/testapi.js'
 import { updComments } from './modules/comments_array.js'
+import { commentsLoader } from './modules/comments_loader.js'
+
+commentsLoader()
 fetchComments().then((data) => {
     updComments(data)
     renderFunction()
---
+++ modules/comments_loader.js
@@ -0,0 +1,5 @@
+export const commentsLoader = () => {
+    const findList = document.querySelector('.comments')
+
+    findList.innerHTML = 'Пожалуйста, подождите, загружаю пкомментарии ...'
+}
